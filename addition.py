29268ac15cf36a5c4529f930e767c07b296736c1
santoshernandezr <santoshernandezr@berea.edu>
2020-02-03 14:43:26 -0500

    Made a function to make a matrix with the users input.

--- addition.py
+++ addition.py
@@ -1,17 +1,44 @@
-firstMatrix = [[1, 2, 3],
-               [4, 5, 6],
-               [5, 6, 7]]
-secondMatrix = [[4, 7, 3],
-                [8, 6, 8],
-                [9, 1, 7]]
-print(firstMatrix)
-print(secondMatrix)
-finalMatrix = []
-for i in range(len(firstMatrix)):
-    matrix = []
-    for j in range(len(secondMatrix[0])):
-        sumNum = firstMatrix[i][j] + secondMatrix[i][j]
-        matrix.append(sumNum)
-        print(matrix)
-    finalMatrix.append(matrix)
-print(finalMatrix)
+def makeMatrix():
+    """
+    This function asks the user to input their matrix
+    :return: this function returns a list
+    """
+    rows = int(input("Please input the number of rows you'd like "))
+    columns = int(input("Please input the number of columns you'd like (it has to be the same as your number of rows "))
+    finalMatrix = []
+    for i in range(rows):
+        matrix = []
+        for j in range(columns):
+            matrix.append(int(input("Please enter the numbers in the rows of the matrix ")))
+        finalMatrix.append(matrix)
+    return finalMatrix
+
+
+def matrixaddition():
+    """
+    This function adds matrices of same dimensions
+    :return: this function returns a list
+    """
+    print("Please enter your first matrix ")
+    firstMatrix = makeMatrix()
+    print("Please enter your second matrix ")
+    secondMatrix = makeMatrix()
+    if len(firstMatrix) == len(secondMatrix):
+        print(firstMatrix)
+        print(secondMatrix)
+        finalMatrix = []
+        for i in range(len(firstMatrix)):
+            matrix = []
+            for j in range(len(secondMatrix[0])):
+                sumNum = firstMatrix[i][j] + secondMatrix[i][j]
+                matrix.append(sumNum)
+                print(matrix)
+            finalMatrix.append(matrix)
+        print(finalMatrix)
+    else:
+        print("This operation cannot be done because the dimensions of the matrices are not the same. "
+              "Try again :)")
+        matrixaddition()
+
+
+matrixaddition()

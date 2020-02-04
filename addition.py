ac34b93386115a7e1b4d1c1968319ae1f16ff88f
santoshernandezr <santoshernandezr@berea.edu>
2020-02-03 19:52:31 -0500

    Documented code.

--- addition.py
+++ addition.py
@@ -5,12 +5,12 @@ def makeMatrix():
     """
     rows = int(input("Please input the number of rows you'd like "))
     columns = int(input("Please input the number of columns you'd like (it has to be the same as your number of rows "))
-    finalMatrix = []
-    for i in range(rows):
-        matrix = []
-        for j in range(columns):
-            matrix.append(int(input("Please enter the numbers in the rows of the matrix ")))
-        finalMatrix.append(matrix)
+    finalMatrix = []  # List that will include the complete matrix
+    for i in range(rows):  # This for loop does a loop for the amount of rows there are
+        matrix = []  # List that will contain the rows of the matrix
+        for j in range(columns):  # This loop does a loop for the amount of columns there are
+            matrix.append(int(input("Please enter the numbers in the rows of the matrix ")))  # This is appending the values the user is inputing
+        finalMatrix.append(matrix)  # Appends each row of the matrix
     return finalMatrix
 
 
@@ -20,21 +20,21 @@ def matrixaddition():
     :return: this function returns a list
     """
     print("Please enter your first matrix ")
-    firstMatrix = makeMatrix()
+    firstMatrix = makeMatrix()  # The first matrix is created by the user
     print("Please enter your second matrix ")
-    secondMatrix = makeMatrix()
-    if len(firstMatrix) == len(secondMatrix):
+    secondMatrix = makeMatrix()  # The second matrix is created by the user
+    if len(firstMatrix) == len(secondMatrix):  # Checks whether the matrices can be added or not
         print(firstMatrix)
         print(secondMatrix)
-        finalMatrix = []
-        for i in range(len(firstMatrix)):
-            matrix = []
-            for j in range(len(secondMatrix[0])):
-                sumNum = firstMatrix[i][j] + secondMatrix[i][j]
+        finalMatrix = []  # Vector that will contain the full matrix
+        for i in range(len(firstMatrix)):  # Will through each row in the matrix
+            matrix = []  # Vector that will get all the rows in the matrix
+            for j in range(len(secondMatrix[0])):  # Will go through each column
+                sumNum = firstMatrix[i][j] + secondMatrix[i][j]  # Will add each individual cell
                 matrix.append(sumNum)
                 print(matrix)
             finalMatrix.append(matrix)
-        print(finalMatrix)
+        print("The sum of the two matrices is: " + str(finalMatrix))
     else:
         print("This operation cannot be done because the dimensions of the matrices are not the same. "
               "Try again :)")
